041cc14a934c5db2915792e8538a7af962b65e88
dhiraj <gdhiraj030@gmail.com>
2026-01-28 15:50:13 +0545

    initialization projects

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    password: { type: String, required: true },
+
+    role: {
+      type: String,
+      enum: ["CUSTOMER", "VENDOR", "RIDER", "ADMIN"],
+      default: "CUSTOMER",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
